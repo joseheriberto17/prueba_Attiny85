3aa0add53545b798d8e709841ef0d49d69eee2ab
joseheriberto17 <jose.heriberto@hotmail.es>
2025-02-23 23:01:08 -0500

    dataej

---
+++ dataw.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=dataw.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/dataw.X.production.hex
