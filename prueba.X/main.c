a69e534f0d78a18efa43941f5f82c5aa761d6fde
joseheriberto17 <jose.heriberto@hotmail.es>
2025-02-23 22:58:40 -0500

    datae

---
+++ prueba.X/main.c
@@ -0,0 +1,28 @@
+/*
+ * File:   main.c
+ * Author: the_pc_heri24
+ *
+ * Created on February 22, 2025, 6:23 AM
+ */
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define LED PB0  // Definimos el pin del LED (en ATmega328P, PB5 suele ser el LED integrado)
+
+int main(void) {
+    // Configuramos el pin LED como salida
+    DDRB |= (1 << LED);
+    
+    while (1) {
+        // Encender el LED
+        PORTB |= (1 << LED);
+        _delay_ms(1000);  // Retardo de 500 ms
+        
+        // Apagar el LED
+        PORTB &= ~(1 << LED);
+        _delay_ms(1000);  // Retardo de 500 ms
+    }
+    
+    return 0;
+}
